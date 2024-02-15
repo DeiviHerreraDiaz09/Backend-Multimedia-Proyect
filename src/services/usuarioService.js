b1fe6563a581b5dd573f801f36bcc1e6896f56e4
Albert Stive <stiven23ospina@gmail.com>
2024-02-15 18:46:53 -0500

    Avances en mensajeria

--- src/services/usuarioService.js
+++ src/services/usuarioService.js
@@ -1,6 +1,6 @@
 import { conexionBD } from "../config/conexion.js";
 import { encriptarClave } from "../helper/encriptacion.js";
-
+import { enviarCorreo } from "./emailServide.js";
 export const obtenerUsuariosServicio = async () => {
   const conexion = await conexionBD();
   const [usuarios, campos] = await conexion.query("SELECT * FROM usuario");
@@ -24,17 +24,41 @@ export const obtenerUsuarioServicio = async (id) => {
 };
 
 export const crearUsuarioServicio = async (usuario) => {
-  const conexion = await conexionBD();
-  const clave = await encriptarClave("12345");
+  try {
+    const conexion = await conexionBD();
+    const clave = await encriptarClave("12345");
 
-  const [resultado, campos] = await conexion.execute(
-    "INSERT INTO usuario (nombre, apellido, correo, clave, rol) VALUES (?,?,?,?,?)",
-    [usuario.nombre, usuario.apellido, usuario.correo, clave, usuario.rol]
-  );
-  const idUsuarioInsertado = resultado.insertId;
-  const obtenerUsuario = await obtenerUsuarioServicio(idUsuarioInsertado);
-  conexion.release();
-  return obtenerUsuario;
+    const [resultado, campos] = await conexion.execute(
+      "INSERT INTO usuario (nombre, apellido, correo, clave, rol) VALUES (?,?,?,?,?)",
+      [usuario.nombre, usuario.apellido, usuario.correo, clave, usuario.rol]
+    );
+    const idUsuarioInsertado = resultado.insertId;
+
+    const [datosUsuario] = await conexion.execute(
+      "SELECT * FROM usuario WHERE id = ?",
+      [idUsuarioInsertado]
+    );
+
+    const opcionesEmail = {
+      from: "PONER CORREO",
+      to: usuario.correo,
+      subject: "Usuario creado 🆕",
+      template: "newUser",
+      context: {
+        nombre: usuario.nombre,
+        correo: usuario.correo,
+      }
+    };
+
+    enviarCorreo(opcionesEmail);
+
+    conexion.release();
+
+    return datosUsuario.length ? datosUsuario[0] : null;
+  } catch (error) {
+    console.error("Error al crear usuario:", error);
+    throw error;
+  }
 };
 
 export const actualizarUsuarioServicio = async (usuarioUpdate, id) => {
